9d15e8eacc7deb4e5f7b547aab3cdb015305c6e3
fengqi <2607546441@qq.com>
2018-12-12 19:44:27 +0800

    加入线检测和旋转

--- PredictCard/main.cpp
+++ PredictCard/main.cpp
@@ -7,9 +7,11 @@ using namespace std;
 
 int main(){
 	int suit = 0, rank[4] = { 0, 0, 0, 0 };
-	for (int i = 0; i < 4; i++){
-		scan(suit, rank[i]);
-		cout << "suit: " << suit << ", rank: " << rank[i] << endl;
+	//for (int i = 0; i < 4; i++){
+	while(1){
+		scan(suit, rank[1]);
+		cout << "suit: " << suit << ", rank: " << rank[1] << endl;
+		waitKey();
 	}
 	D24(rank[0], rank[1], rank[2], rank[3]);
 	return 0;
